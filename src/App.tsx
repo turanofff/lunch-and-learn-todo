bc96faecf2b68686c6c8b77719fc64c082764574
turanofff <git@stackblitz.com>
2025-09-19 13:56:00 +1000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+const App = () => {
+  return (
+    <>
+      <h1> My First Todo React App </h1>
+      <p> Mark items as complete or add more items to the list</p>
+      <br />
+      <div>
+        <i> Start coding away and bring this project to live!</i>
+      </div>
+    </>
+  );
+};
+
+export default App;
